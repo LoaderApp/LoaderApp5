5a75858aa09a3292e9b9f833a5284404ae67d93e
Raouf <RaoufManzoor@yahoo.com>
2018-04-10 00:26:45 +0500

    commit

---
+++ LoaderApp6/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace LoaderApp6.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
---
+++ LoaderApp6/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LoaderApp6.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
